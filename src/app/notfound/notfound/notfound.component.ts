19c4af1908614472d5e2d03342b23bd9b8a6530b
Janith Ranasinghe <janithraditha@gmail.com>
2024-04-27 18:36:10 +0530

    add notfound page

---
+++ src/app/notfound/notfound/notfound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound',
+  standalone: true,
+  imports: [],
+  templateUrl: './notfound.component.html',
+  styleUrl: './notfound.component.scss'
+})
+export class NotfoundComponent {
+  imgUrl='https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png';
+}
