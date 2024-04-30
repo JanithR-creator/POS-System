5a03003e901b31a728e9a5bca50c1d43a3819fa0
Janith Ranasinghe <janithraditha@gmail.com>
2024-04-30 08:53:34 +0530

    make lots of updates and implement product context component

--- src/app/components/product-context/product-context.component.ts
+++ src/app/components/product-context/product-context.component.ts
@@ -1,29 +1,49 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf} from "@angular/common";
 import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {AsyncPipe, CurrencyPipe, NgForOf, NgIf} from "@angular/common";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
-  selector: 'app-product-context',
+  selector: 'app-product-all',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    RouterLink,
+    CurrencyPipe,
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './product-context.component.html',
   styleUrl: './product-context.component.scss'
 })
 export class ProductContextComponent implements OnInit{
-  constructor(private db:AngularFirestore) {
+
+  constructor(private db: AngularFirestore,
+              private storage: AngularFireStorage,) {
+
   }
 
-  productList:any[]=[];
+  productList: any[] = [];
+
   ngOnInit(): void {
     this.db.collection('products').get().subscribe(
-      quarySnapShot=>{
-        quarySnapShot.forEach(doc=>{
-          this.productList.push(doc.data());
+      querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.productList.push({id: doc.id, data: doc.data()});
         })
-      }
-    )
+      })
   }
 
+  deleteCustomer(id: any, avatar: any) {
+    if (confirm('are you sure?')) {
+      this.db.collection('products').doc(id).delete();
+      this.storage.storage.refFromURL(avatar).delete();
+    }
+  }
 }
